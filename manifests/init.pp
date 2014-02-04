ef74e14b9a5e3f75f3d79c7b9faf1e57203352d4
hkumarmk <hkumarmk@gmail.com>
2014-02-04 19:52:12 +0000

    added code to set function based dns cname to the servers, it set backend interface for compute and frontend for storage nodes
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/sethostname@24 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,17 @@
 class sethostname (
-	$domain_name = "mu.jio"
+	$domain_name,
+	$dns_master_server,
+	$dnsupdate_key
 ) {
-    $nic = 'eth1'
+  $nic = 'eth1'
+  $hgs_to_use_fe_for_connectivity	= ['storage_node','database_node','contrail_node','cobbler_node','repo_node']
+#  $hgs_to_use_fe_for_connectivity=
+
+  if $::hostgroup_from_dns in $hgs_to_use_fe_for_connectivity {
+	$dns_host_name_part =  regsubst($dnsname_from_dns,'^(\w\w\d\d\d\d)(\w)(-.*)','\1f\3','I')
+  } else {
+	$dns_host_name_part =  regsubst($dnsname_from_dns,'^(\w\w\d\d\d\d)(\w)(-.*)','\1b\3','I')
+  }
     file { "/etc/hosts":
 	ensure => present,
         owner => root,
@@ -30,7 +40,7 @@ class sethostname (
 server $dns_master_server
 key dhcpupdate $dnsupdate_key
 zone $domain_name
-update add ${hostname_from_dns}.${domain_name} 86400 IN CNAME ${dnsname_from_dns}.${domain_name}
+update add ${hostname_from_dns}.${domain_name} 86400 IN CNAME ${dns_host_name_part}.${domain_name}
 send
 EOF
 ",
