e9b717c63eb6d9cfedc6dc77591f990d679ae4a4
amar266 <amar266@gmail.com>
2014-03-31 06:01:37 +0000

    Changed in multiple file for hostgroup_from_dns variable
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/sethostname@401 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- lib/facter/hostgroup_from_dns.rb
+++ lib/facter/hostgroup_from_dns.rb
@@ -1,13 +1,13 @@
-Facter.add("hostgroup_from_dns") do
-    setcode do
-	#ip_eth = (Facter.value(:ipaddress_eth2) || Facter.value(:ipaddress_eth3)) || Facter.value(:ipaddress_eth0)
-	ip_eth = (Facter.value(:ipaddress_eth2) || Facter.value(:ipaddress_eth3)) || ( Facter.value(:ipaddress_eth0) || Facter.value(:ipaddress_vhost0)) || Facter.value(:ipaddress_br0)
+#Facter.add("hostgroup_from_dns") do
+#    setcode do
+#	#ip_eth = (Facter.value(:ipaddress_eth2) || Facter.value(:ipaddress_eth3)) || Facter.value(:ipaddress_eth0)
+#	ip_eth = (Facter.value(:ipaddress_eth2) || Facter.value(:ipaddress_eth3)) || ( Facter.value(:ipaddress_eth0) || Facter.value(:ipaddress_vhost0)) || Facter.value(:ipaddress_br0)
 # || Facter.value(:ipaddress_eth0)
-	current_hostname = Facter.value(:hostname)
+#	current_hostname = Facter.value(:hostname)
 #	Facter::Util::Resolution.exec("host  #{ip_eth}").chomp
 	#Facter::Util::Resolution.exec("host #{ip_eth}").sub(/.*pointer/,'').gsub(/(\w)[BFbf]*\..*/,'\1').sub(/\w+-(\w)/,'\1').gsub(/\s/,'').sub(/.*addr.arpa.notfound.*/,"#{current_hostname}").sub(/.*connectiontimedout.*/,"#{current_hostname}").sub(/^oc\d+$/,"Openstack Controller").sub(/^st\d+$/,"Storage Node").sub(/^cp\d+$/,"Openstack Compute Node").sub(/^db\d+$/,"Database Node").sub(/^ct\d+$/,"Contrail Node").sub(/^z\d+/,"Zabbix Node").sub(/^i\d+/,"Infra Node").sub(/^cblr\d*/,"Cobbler Node").sub(/^repo\d*/,"Repo Node").chomp
-	Facter::Util::Resolution.exec("host #{ip_eth}").sub(/.*pointer/,'').gsub(/(\w)[BFbf]*\..*/,'\1').sub(/\w+-(\w)/,'\1').gsub(/\s/,'').sub(/.*addr.arpa.notfound.*/,"#{current_hostname}").sub(/.*connectiontimedout.*/,"#{current_hostname}").sub(/^oc\d+$/,"os_controller_node").sub(/^st\d+$/,"storage_node").sub(/^cp\d+$/,"os_compute_node").sub(/^db\d+$/,"database_node").sub(/^ct\d+$/,"contrail_node").sub(/^z\d+/,"zabbix_node").sub(/^i\d+/,"infra_node").sub(/^cblr\d*/,"cobbler_node").sub(/^repo\d*/,"repo_node").chomp
+#	Facter::Util::Resolution.exec("host #{ip_eth}").sub(/.*pointer/,'').gsub(/(\w)[BFbf]*\..*/,'\1').sub(/\w+-(\w)/,'\1').gsub(/\s/,'').sub(/.*addr.arpa.notfound.*/,"#{current_hostname}").sub(/.*connectiontimedout.*/,"#{current_hostname}").sub(/^oc\d+$/,"os_controller_node").sub(/^st\d+$/,"storage_node").sub(/^cp\d+$/,"os_compute_node").sub(/^db\d+$/,"database_node").sub(/^ct\d+$/,"contrail_node").sub(/^z\d+/,"zabbix_node").sub(/^i\d+/,"infra_node").sub(/^cblr\d*/,"cobbler_node").sub(/^repo\d*/,"repo_node").chomp
 	#Facter::Util::Resolution.exec("echo hello").chomp
-    end
-end
+#    end
+#end
 
